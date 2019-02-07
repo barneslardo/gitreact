32ce1e5de44e663e71e65cee877a4192324b85cc
barneslardo <37343575+barneslardo@users.noreply.github.com>
2019-02-06 19:56:06 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Security } from "@okta/okta-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const oktaConfig = {
+  issuer: process.env.REACT_APP_OKTA_ORG_URL,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Security {...oktaConfig}>
+      <App />
+    </Security>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+serviceWorker.register();
+
+if (module.hot) module.hot.accept();
